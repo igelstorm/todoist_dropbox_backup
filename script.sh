d451b9d371c4e498c6a8dc21aab39665f1e575ac
Erik Igelström <erik@cultivatehq.com>
2018-01-06 12:53:00 +0000

    Upload a test file to Dropbox

--- script.sh
+++ script.sh
@@ -2,8 +2,15 @@
 
 source ./secret.env
 
-backup_url=$(curl https://todoist.com/api/v7/backups/get \
-    -d token=$TODOIST_TOKEN \
-    | jq '.[0].url')
+# backup_url=$(curl https://todoist.com/api/v7/backups/get \
+#     -d token=$TODOIST_TOKEN \
+#     | jq '.[0].url')
 
-echo $backup_url
+# echo $backup_url
+
+curl -X POST https://content.dropboxapi.com/2/files/upload \
+    --header "Authorization:Bearer $DROPBOX_TOKEN" \
+    --header "Dropbox-API-Arg: {\"path\": \"/test.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary @test.txt
+
