41c0f76521407f91455df8280387e955a9e33dce
Erik Igelström <erik@cultivatehq.com>
2018-01-06 13:13:20 +0000

    Add timestamp to filename

--- script.sh
+++ script.sh
@@ -12,11 +12,12 @@ echo ""
 echo "Downloading backup file from Todoist"
 curl $backup_url > $filename
 
+timestamp=$(date "+%Y-%m-%d")
 echo ""
 echo "Uploading backup file to Dropbox"
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization:Bearer $DROPBOX_TOKEN" \
-    --header "Dropbox-API-Arg: {\"path\": \"/$filename\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
+    --header "Dropbox-API-Arg: {\"path\": \"/$timestamp-$filename\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary @$filename
 
